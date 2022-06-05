7d4b831c89cca465050460ee9946374037dd86b8
Phil Smith <philip@philipsmith.ie>
2022-06-05 10:43:38 +0100

    Use useLazyQuery hook and handle address changes.

--- src/App.tsx
+++ src/App.tsx
@@ -3,46 +3,75 @@ import { BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header';
 import Container from './components/Container';
 import Footer from './components/Footer';
-import { gql, useQuery } from '@apollo/client';
+import { DocumentNode, gql, useLazyQuery, useQuery } from '@apollo/client';
 import AssetCard from './components/AssetCard';
 import { Erc1155Balance, Erc1155Token } from './erc';
+import Box from '@mui/material/Box/Box';
+import { useEffect, useState } from 'react';
+import { CircularProgress, LinearProgress } from '@mui/material';
 
 const TOKENS = gql`
-  query getAccountERC1155Balances {
-  accounts(where: {id:"0x8d408adf3416f038ef48cf3093023b3fd7fc9bb5"}) {
-    ERC1155balances {
-      token {
-        id
-        uri
-      }
-      valueExact
-    }
-  }
-}`;
+        query getAccountERC1155Balances($address:String!) {
+        account(id:$address) {
+          ERC1155balances {
+            token {
+              id
+              uri
+            }
+            valueExact
+          }
+        }
+      }`
 
 function App() {
-  const { loading, error, data } = useQuery(TOKENS);
-  if(data) {
-    console.log('data is: ', data);
-    console.log('accounts is', data.accounts)
-    console.log('balances is ', data.accounts[0].ERC1155balances);
-  }
-  return (
-<BrowserRouter>
 
-      <Header title='Gala Games Token History' />
-      
-      <Container pageWidth={1100} flexDirection={'row'} justifyContent={'center'}>
-      {data !== undefined && (
-        data.accounts[0].ERC1155balances?.map((balance:Erc1155Balance, i:number) => (
-          <AssetCard key={i.toString()} token={balance.token} valueExact={balance.valueExact} />
-        )
-      ))}
-      </Container>
+  const [address, setAddress] = useState<string | undefined>();
+  const [showLoading, setShowLoading] = useState(false);
+  let [getTokens, { called, loading , data }] = useLazyQuery(TOKENS)
+
+  
+  const onAddressChanged = (address:string)=>{
+    setShowLoading(true);
+    console.log(`address changed to ${address}`)
+    getTokens({variables: { address }})
+  }
+
+  useEffect(()=> {
+    console.log(`loading is ${loading} called is ${called}`)
+    if(called && !loading) {
+      setShowLoading(false)
+    }
+  }, [loading, called])
+
+  useEffect(()=>{
+    console.log(`showLoading is ${showLoading}`)
+  }, [showLoading])
+
+  return (
+    <BrowserRouter>
+
+      <Header title='Gala Games Token History' onAddressChanged={onAddressChanged} />
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Container pageWidth={1100} flexDirection={'row'} justifyContent={'center'}>
+          {showLoading && (
+            <CircularProgress />
+          )}
+          {data !== undefined && (
+            data.account.ERC1155balances?.map((balance: Erc1155Balance, i: number) => (
+              <AssetCard key={i.toString()} token={balance.token} valueExact={balance.valueExact} />
+            )
+          ))}
+        </Container>
+      </Box>
       <Footer />
 
-</BrowserRouter>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
