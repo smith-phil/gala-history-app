5eb12bb8f5b6fac79bc99202b173f35b5b22885a
Phil Smith <philip@philipsmith.ie>
2022-06-05 13:00:38 +0100

    Add doc comments to ErrorAlert

--- src/components/ErrorAlert.tsx
+++ src/components/ErrorAlert.tsx
@@ -1,11 +1,20 @@
 import { styled } from "@mui/material";
 import Alert from "@mui/material/Alert/Alert"
 
+/**
+ * @property message, the message to display
+ * @property onClose, callback to execute when alert closes
+ */
 interface ErrorAlertProps {
     message:string,
     onClose:()=>void
 }
 
+/**
+ * 
+ * @param props {@link ErrorAlertProps}
+ * @returns Alert showing an error 
+ */
 export const ErrorAlert = (props:ErrorAlertProps) => {
         return (
             <Alert
