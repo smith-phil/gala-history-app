74ef85b9992feadf070be17e9ac3ec9aab78cc30
Phil Smith <philip@philipsmith.ie>
2022-06-07 23:09:26 +0100

    Update the footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,42 +1,39 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import styled from '@emotion/styled';
-//import { colors, ApolloIcon } from '../styles';
+import { Link } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 
 type FooterProps = {
-    children?:ReactNode[]
+  children?: ReactNode[]
 }
+
 /**
- * Footer is useless component to make our app look a little closer to a real website!
+ * Generic footer for the website
  */
-const Footer = (props:FooterProps) => {
+const Footer = (props: FooterProps) => {
   return (
-    <FooterContainer>
-      Gala Games content © 2021 Gala Games
-      {props.children}
-    </FooterContainer>
+    <footer style={{ width: '100%', height: '70px', paddingTop: '10px', paddingBottom: '10px', backgroundColor: '#1976d2', display: 'flex', flexDirection: 'column' }}>
+      <div style={{ margin: 'auto', color: '#fff', lineHeight: '1vh' }}>Galawallet.info is an unofficial site and has no connection to or affiliation with Gala Games</div>
+      <FooterContent>
+        <div>Gala Games content © 2021 <Link href='https://app.gala.games' color='inherit' underline='hover'>Gala Games</Link></div>
+        <div><Link href='tandc.html' color='inherit' underline='hover'>Terms and Conditions</Link></div>
+        <div><GitHubIcon fontSize='inherit' />&nbsp;<Link href="https://github.com/smith-phil/gala-wallet-app" color='inherit' underline='hover'>Source Code</Link> </div>
+      </FooterContent>
+    </footer>
   );
 };
 
 export default Footer;
 
-/** Footer styled components */
-const FooterContainer = styled.div<FooterProps>`
-display: flex,
-flexDirection: row,
-justifyContent: center,
-alignItems: center,
-color: pink,
-marginTop: 30,
-height: 200,
-padding: 20,
-backgroundColor: white,
-borderTop: solid 1px pink,`
+const FooterContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 70%;
+  margin-right: auto;
+  margin-left: auto;
+  justify-content: space-evenly;
+  align-items: baseline;
+  color: #fff;`
 
-const LogoContainer = styled.div({
-  height: 40,
-  marginLeft: 5,
-  svg: {
-    height: 40,
-  },
-});
+
